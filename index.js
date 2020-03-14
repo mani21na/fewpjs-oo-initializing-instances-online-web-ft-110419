618509bb3a50f8c9c6fa85286d6ca0b5a1bdf300
Hanna Lee <mani21na@gmail.com>
2020-03-14 22:47:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,5 +6,18 @@ class Breakfest {
 }
 
 class Luch {
-  constructor(salad, soup, drink)
+  constructor(salad, soup, drink) {
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
+  }
+}
+
+class Dinner {
+  constructor(salad, soup, entree, dessert) {
+    this.salad = salad;
+    this.soup = soup;
+    this.entree = entree;
+    this._dessert = dessert;
+  }
 }
